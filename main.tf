791127c580534ade2d72a2658d6aaa80fe56102c
Akhila Naveen <akhilanaveen@Akhilas-MacBook-Pro.local>
2026-07-17 18:06:18 -0500

    okta-pipeline-changes

---
+++ main.tf
@@ -0,0 +1,41 @@
+# Define Provider Here
+
+terraform {
+    required_providers {
+        okta = {
+            source = "okta/okta"
+            version = "~> 4.0"
+        }
+    }
+}
+
+# Create a okta group resource 
+
+resource "okta_group" "engineering" {
+    name = "Engineering-All"
+    description = "Managed Via Terraform - contains all Engineering staff"
+}
+
+# create another group resource 
+
+resource "okta_group" "productowners" {
+    name = "Productowners-All"
+    description = "Managed Via Terraform - contains all Productowners staff"
+}
+
+# create a custom portal
+
+resource "okta_app_oauth" "custom_portal"{
+    label = "Internal App"
+    type  = "web"
+    grant_type = ["authorization_code"]
+    redirect_uris = ["https://localhost:8080/login/callback"]
+    response_types = ["code"]
+}
+
+# Assign group to okta application
+
+resource "okta_app_group_assignment" "eng_portal_access" {
+    app_id  = okta_app_oauth.custom_portal.app_id
+    group_id = okta_group.engineering.id
+}
